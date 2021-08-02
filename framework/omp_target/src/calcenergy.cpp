6bbd1e539e11d9f89152e9a9717fa5f051504472
Mathialakan Thavappiragasam <mathialakan@gmail.com>
2021-08-02 00:46:48 -0400

    changes to work for nvchpc

--- framework/omp_target/src/calcenergy.cpp
+++ framework/omp_target/src/calcenergy.cpp
@@ -23,7 +23,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
 #include "kernels.hpp"
-#include "calcenergy_gpu.hpp"
+#include "calcenergy.hpp"
 
 //#define DEBUG_ENERGY_KERNEL
 
